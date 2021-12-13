36ddd8dfd17b6d1fd74c49d6ca007060d1e7a11f
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-13 10:54:06 +0600

    full code

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
-// This is a public sample test API key.
+// This is a public sample test API key. Code from Stripe Official Documentation
 // To avoid exposing it, don't submit any personally identifiable information through requests with this API key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
 const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static("public"));
 
-const YOUR_DOMAIN = "http://localhost:4242";
+//declare public
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/create-checkout-session", async (req, res) => {
+//config dotenv
+require("dotenv").config();
+
+const YOUR_DOMAIN = process.env.DOMAIN;
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
+
+app.use(async (req, res, next) => {
+    //create a product
     const product = await stripe.products.create({ name: "T-shirt" });
-    console.log(product);
+
+    //create a price id
     const price = await stripe.prices.create({
         product: product.id,
         unit_amount: 2000,
         currency: "usd",
     });
+
+    req.locals = {
+        price,
+    };
+    next();
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+    //extract the price from locals
+    const { price } = req.locals;
+
+    //start a stripe session
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
@@ -25,11 +46,17 @@ app.post("/create-checkout-session", async (req, res) => {
             },
         ],
         mode: "payment",
+
+        //set succss url to session url
         success_url: `${YOUR_DOMAIN}/success.html`,
+
+        //set failure url to session url
         cancel_url: `${YOUR_DOMAIN}/cancel.html`,
     });
 
+    //redirect to session url
     res.redirect(303, session.url);
 });
 
+//start server
 app.listen(4242, () => console.log("Running on port 4242"));
